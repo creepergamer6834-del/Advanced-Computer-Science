366319c1e05dc37b017ba5df5eb01bbcb8c2acb3
Sebastian L <creepergamer6834@gmail.com>
2026-08-25 13:59:17 -0700

    Finished 0.1 Assignment

---
+++ Unit 1 Day 1Challenge/AboutMe.java
@@ -0,0 +1,9 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("Name: Sebastian Lee\n" + //
+                "Birthday: July 17, 2011\n" + //
+                "Hobbies: Robotics, Science, Photography\n" + //
+                "Favorite Book: War and Peace\n" + //
+                "Favorite Movie: Englorious Basterds");
+    }
+}
---
+++ Unit 1 Day 1Challenge/BlockLetters.java
@@ -0,0 +1,15 @@
+public class BlockLetters {
+    public static void main(String[] args) {
+        System.out.println(" A\n" + //
+                "SSS\n" + //
+                "S  \n" + //
+                "SSS\n" + //
+                "  S\n" + //
+                "SSS\n" + //
+                "L\n" + //
+                "L\n" + //
+                "L\n" + //
+                "L\n" + //
+                "LLL");
+    }
+}
