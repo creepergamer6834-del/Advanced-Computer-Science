6a13c235fd8e62321fa3720dc596ceb941496bf8
Sebastian L <creepergamer6834@gmail.com>
2026-08-25 13:27:00 -0700

    Class

---
+++ Unit 1 Day 1/Dog.java
@@ -0,0 +1,16 @@
+public class Dog {
+    public static void main(String[] args) {
+        System.out.println("woof");
+        System.out.println("bark\nbark");
+        System.out.println("This dog has a stick. \nIt's a good boy!");
+        // \n is an Escape character just like \t — Inserts a tab. \b — Inserts a backspace. \n —
+        // Inserts a newline. \r — Inserts a carriage return. \f — Inserts a form feed. \' — Inserts
+        // a single quote. \" — Inserts a double quote. \\ — Inserts a backslash.
+        System.out.println("Name:\tGrace");
+        System.out.println("Has Tail:\tyes\nColor:\tbrown");
+        // this is a human for computer consumption
+        // *this is a multi line comment you can write
+        // an entire paragraph without the double
+        // backslash */
+    }
+}
---
+++ Unit 1 Day 1/Lesson.java
@@ -0,0 +1,6 @@
+public class Lesson {
+    // method -> main method entry point for the Java program
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
