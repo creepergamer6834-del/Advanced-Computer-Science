6c4fe9e99c0f31214ed85e86b5fd38b2ea626e0e
Sebastian L <creepergamer6834@gmail.com>
2026-08-27 13:08:36 -0700

    Finished stretch

---
+++ Setup Check/SetupCheck.java
@@ -0,0 +1,6 @@
+public class SetupCheck {
+    public static void main(String[] args) {
+        System.out.println("Name: Sebastian Lee");
+        System.out.println("ACS setup complete");
+    }
+}
---
+++ Unit 1 Day 2/VariablesAndPrimitives.java
@@ -0,0 +1,22 @@
+public class VariablesAndPrimitives {
+    /// primitive data type
+    /// 8 data types
+
+    ///int - integer (-3,0,6,7)
+    //double - decimal number (float in python) real number
+    //boolean - true/false
+    //char - "a" , "b"
+    //byte - 00000000 8 code that things get translated into
+    //short
+    //long
+
+    // 'a' -> 97 -> 11000001
+    // 'A' -> 65 -> 
+
+    //string - not primitive, are a collection/list of characters its a class
+    int score = 85;
+    double price = 10.99;
+    boolean isPassing = true;
+    char tier = 'S';
+    
+}
